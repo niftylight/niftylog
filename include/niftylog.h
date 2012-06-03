c1d0059be579408e45ed72d22c6875ea90410581
daniel <info-niftylog@niftylight.de>
2012-06-03 23:35:30 +0200

    changed 2010 copyright statement to 2012

--- include/niftylog.h
+++ include/niftylog.h
@@ -1,6 +1,6 @@
 /*
  * libniftylog - niftylight logging library 
- * Copyright (C) 2006-2010 Daniel Hiepler <daniel@niftylight.de>
+ * Copyright (C) 2006-2012 Daniel Hiepler <daniel@niftylight.de>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
